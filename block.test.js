37fe6581d7d0b64a059803ecdb1f18f9b375391c
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-10-16 21:01:24 -0500

    Adjust difficulty in mineBlock with a static method

--- block.test.js
+++ block.test.js
@@ -93,11 +93,17 @@ describe('Block', () => {
         timestamp: block.timestamp + MINE_RATE - 100
       })).toEqual(block.difficulty + 1);
     });
+
     it('lowers the difficulty for a slowly mined block', () => {
       expect(Block.adjustDifficulty({
         originalBlock: block,
         timestamp: block.timestamp + MINE_RATE + 100
       })).toEqual(block.difficulty - 1);
+    });
+
+    it('has a lower limit of 1', () => {
+      block.difficulty = -1;
+      expect(Block.adjustDifficulty({ originalBlock: block })).toEqual(1);
     });
   });
 });
