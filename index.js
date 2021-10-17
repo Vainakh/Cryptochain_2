d160c5b5b8fba2c5a4bf1f320f680ffc69df7fa1
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-10-17 11:21:53 -0500

    Install body parser

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 
 const app = express();
 const blockchain = new Blockchain();
+
+app.use(bodyParser.json());
 
 app.get('/api/blocks', (req, res) => {
   res.json(blockchain.chain);
