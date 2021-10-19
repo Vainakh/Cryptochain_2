98bbd58539503d07174e114dcab0f23522a994d4
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-10-19 12:08:49 -0500

    Update transaction with multiple outputs tdd

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -87,6 +87,41 @@ describe('Transaction', () => {
           expect(errorMock).toHaveBeenCalled();
         });
       });
+
+
+    });
+  });
+
+  describe('update', () => {
+    let originalSignature, originalSenderOutput, nextRecipient, nextAmount;
+
+    beforeEach(() => {
+      originalSignature = transaction.input.signature;
+      originalSenderOutput = transaction.outputMap[senderWallet.publicKey];
+      nextRecipient = 'next-recipient';
+      nextAmount = 50;
+
+      transaction.update({ senderWallet, recipient: nextRecipient, amount: nextAmount });
+    });
+
+    it('outputs the amount to the next recipient', () => {
+      expect(transaction.outputMap[nextRecipient]).toEqual(nextAmount);
+    });
+
+    it('substructs the amount from the original sender output amount', () => {
+      expect(transaction.outputMap[senderWallet.publicKey])
+        .toEqual(originalSenderOutput - nextAmount);
+    });
+
+    it('maintains the total output that matches the input amount', () => {
+      expect(
+        Object.values(transaction.outputMap)
+          .reduce((total, outputAmount) => total + outputAmount)
+      ).toEqual(transaction.input.amount);
+    });
+
+    it('re-signs the transaction', () => {
+      expect(transaction.input.signature).not.toEqual(originalSignature);
     });
   });
 });
