ab071b4ae66ef8ce4e15cefc027f0f41f6b03ac8
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-10-18 20:29:55 -0500

    Create Transaction class and add create output method on it to generate outputMap object

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,9 +1,23 @@
+const uuid = require('uuid/v1');
+
 class Transaction {
   constructor({ senderWallet, recipient, amount }) {
-    this.senderWallet = senderWallet;
-    this.recipient = recipient;
-    this.amount = amount;
+    this.id = uuid();
+    this.outputMap = this.createOutputMap(
+      {
+        senderWallet,
+        recipient,
+        amount
+      }
+    );
   }
+
+  createOutputMap({ senderWallet, recipient, amount }) {
+    const outputMap = {};
+    outputMap[recipient] = amount;
+    outputMap[senderWallet.publicKey] = senderWallet.balance - amount;
+    return outputMap;
+  };
 }
 
 module.exports = Transaction;
