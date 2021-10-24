4968c1a54abe7da0204453f11c510a0b01ff0ff3
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-10-23 19:07:46 -0500

    Add mine transactions api

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+
 const request = require('request');
+
 const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
 const TransactionPool = require('./wallet/transaction-pool');
 const Wallet = require('./wallet');
+const TransactionMiner = require('./app/transaction-miner');
 
 const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain, transactionPool });
+const transactionMiner = new TransactionMiner({ blockchain, transactionPool, wallet, pubsub });
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -49,6 +53,11 @@ app.post('/api/transact', (req, res) => {
 
 app.get('/api/transaction-pool-map', (req, res) => {
   res.json(transactionPool.transactionMap);
+});
+
+app.get('/api/mine-transactions', (req, res) => {
+  transactionMiner.mineTransactions();
+  res.redirect('/api/blocks');
 });
 
 const syncWithRootState = () => {
