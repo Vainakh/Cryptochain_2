f002a9eefe7ae43070564b656eafa7429fd0a466
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-10-24 19:33:24 -0500

    Calculate wallet balance tss

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -69,6 +69,19 @@ describe('Wallet', () => {
         expect(transaction.outputMap[recipient]).toEqual(amount);
       });
     });
+
+    describe('and a chain is passed', () => {
+      it('calls `Wallet.calculateBalance', () => {
+        const calculateBalanceMock = jest.fn();
+        Wallet.calculateBalance = calculateBalanceMock;
+        wallet.createTransaction({
+          recipient: 'foo',
+          amount: 50,
+          chain: new Blockchain().chain
+        });
+        expect(calculateBalanceMock).toHaveBeenCalled();
+      });
+    });
   });
 
   describe('calculateBalance()', () => {
