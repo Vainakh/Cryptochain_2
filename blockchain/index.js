9d7c64bc66f43044eca70157e3aa0cdb295e0228
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-10-25 14:56:21 -0500

    Finish valid transaction data

--- blockchain/index.js
+++ blockchain/index.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const Transaction = require('../wallet/transaction');
 const { cryptoHash } = require('../util');
 const { REWARD_INPUT, MINING_REWARD } = require('../config');
 
@@ -39,6 +40,16 @@ class Blockchain {
           rewardTransactionCount += 1;
           if (rewardTransactionCount > 1) {
             console.error('Miner rewards exceed limit');
+            return false;
+          }
+
+          if (Object.values(transaction.outputMap)[0] !== MINING_REWARD) {
+            console.error('Miner reward amount is invalid');
+            return false;
+          }
+        } else {
+          if (!Transaction.validTransaction(transaction)) {
+            console.error('Invalid transaction');
             return false;
           }
         }
