d56248e0d1c47d9bbf32f4a4d6e3f4320ad69a2f
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-10-25 16:20:47 -0500

    Add logic to prevent duplicate transactions in the block

--- blockchain/index.js
+++ blockchain/index.js
@@ -35,6 +35,7 @@ class Blockchain {
   validTransactionData({ chain }) {
     for (let i = 1; i < chain.length; i ++) {
       const block = chain[i];
+      const transactionSet = new Set();
       let rewardTransactionCount = 0;
       for (let transaction of block.data) {
         if (transaction.input.address === REWARD_INPUT.address) {
@@ -62,6 +63,13 @@ class Blockchain {
           if (transaction.input.amount !== trueBalance) {
             console.error('Invalid input amount');
             return false;
+          }
+
+          if (transactionSet.has(transaction)) {
+            console.error('An identical transaction appears more than once in the block')
+            return false;
+          } else {
+            transactionSet.add(transaction);
           }
         }
       }
