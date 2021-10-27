611008828fb1a802c86b2a9fdf68ce0d6aa5b540
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-10-26 20:59:24 -0500

    Install and config babel dependencies in package.json and .babelrc file

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    walletInfo: {
+      address: "fooxv6",
+      balance: 9999
+    }
+  };
+
   render() {
+    const { address, balance } = this.state.walletInfo;
+
     return (
       <div>
-        Welcome to the blockchain...
+        <div>Welcome to the blockchain...</div>
+        <div>Address: { address }</div>
+        <div>Balance: { balance }</div>
       </div>
     );
   }
