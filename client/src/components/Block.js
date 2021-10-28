4429f160c7ee34585d2f8c3f0525b875d7fe8c92
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-10-28 18:35:18 -0500

    Add toggle transaction details functionality. Use assessor property approach

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -1,23 +1,65 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 class Block extends Component {
-  render() {
-    const { timestamp, hash, data, id } = this.props.block;
-    const blockNumber = this.props.id === 0 ?
-                        "Genesis Block" :
-                        this.props.id;
-    const hashDisplay = `${hash.substring(0, 15)}...`;
+  state = { displayTransaction: false };
+
+  toggleTransaction = () => {
+    this.setState({ displayTransaction: !this.state.displayTransaction });
+  }
+
+  get displayTransaction() {
+    const { data } = this.props.block;
+
     const stringifiedData = JSON.stringify(data);
     const dataDisplay = stringifiedData.length > 35 ?
           `${stringifiedData.substring(0, 35)}...` :
           stringifiedData;
+
+    if (this.state.displayTransaction) {
+      return (
+          <div>
+              {JSON.stringify(data)}
+              <br />
+              <Button
+              bsStyle="danger"
+              bsSize="small"
+              onClick={this.toggleTransaction}
+            >
+              Show Less
+            </Button>
+          </div>
+        )
+      }
+
+    return (
+      <div>
+        <div>Data: {dataDisplay}</div>
+        <Button
+          bsStyle="danger"
+          bsSize="small"
+          onClick={this.toggleTransaction}
+        >
+          Show More
+        </Button>
+      </div>
+    )
+  }
+
+  render() {
+    console.log('this.displayTransaction', this.displayTransaction)
+    const { timestamp, hash, id } = this.props.block;
+    const blockNumber = this.props.id === 0 ?
+                        "Genesis Block" :
+                        this.props.id;
+    const hashDisplay = `${hash.substring(0, 15)}...`;
 
     return (
       <div className='Block'>
         <div>Id: {blockNumber}</div>
         <div>Hash: {hashDisplay} </div>
         <div>Timestamp: {new Date(timestamp).toLocaleDateString()}</div>
-        <div>Data: {dataDisplay}</div>
+        {this.displayTransaction}
       </div>
     )
   }
