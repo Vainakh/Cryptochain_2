9b12be597744fb4fa8acc53719c5b05f6eba34c2
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-10-28 19:13:32 -0500

    Configure transaction component input and style it

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
+import Transaction from './Transaction';
 
 class Block extends Component {
   state = { displayTransaction: false };
@@ -19,11 +20,18 @@ class Block extends Component {
     if (this.state.displayTransaction) {
       return (
           <div>
-              {JSON.stringify(data)}
+              {
+                data.map(transaction => (
+                  <div key={transaction.id}>
+                    <hr />
+                    <Transaction transaction={transaction}/>
+                  </div>
+                ))
+              }
               <br />
               <Button
-              bsStyle="danger"
-              bsSize="small"
+              bsstyle="danger"
+              bssize="small"
               onClick={this.toggleTransaction}
             >
               Show Less
@@ -36,8 +44,8 @@ class Block extends Component {
       <div>
         <div>Data: {dataDisplay}</div>
         <Button
-          bsStyle="danger"
-          bsSize="small"
+          bsstyle="danger"
+          bssize="small"
           onClick={this.toggleTransaction}
         >
           Show More
