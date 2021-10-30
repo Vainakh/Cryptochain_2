79cd88f65f96e968063adcc1962815fd6266070d
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-10-29 19:45:58 -0500

    Set up react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import Blocks from './Blocks';
-import logo from '../assets/logo.png'
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
 
 class App extends Component {
   state = { walletInfo: {} };
-
   componentDidMount() {
     fetch('http://localhost:3000/api/wallet-info')
       .then(response => response.json())
@@ -18,14 +17,20 @@ class App extends Component {
       <div className="App">
         <img className='logo' src={logo}></img>
         <br/>
-        <div>Welcome to the blockchain...</div>
+        <div>
+          Welcome to the blockchain...
+        </div>
+        <br/>
+          <div>
+            <Link to='/blocks'>
+              Blocks
+            </Link>
+          </div>
         <br/>
         <div className="WalletInfo">
           <div>Address: { address }</div>
           <div>Balance: { balance }</div>
         </div>
-        <br/>
-        <Blocks/>
       </div>
     );
   }
