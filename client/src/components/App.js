ed3afba3a216cabadbece294d690dc04e4bb0e4b
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-10-29 20:59:08 -0500

    Transaction pool component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -21,16 +21,9 @@ class App extends Component {
           Welcome to the <span><strong>Dragon</strong></span> blockchain...
         </div>
         <br/>
-          <div>
-            <Link to='/blocks'>
-              Blocks
-            </Link>
-          </div>
-          <div>
-            <Link to='/conduct-transaction'>
-              Conduct a Transaction
-            </Link>
-          </div>
+          <div><Link to='/blocks'>Blocks</Link></div>
+          <div><Link to='/conduct-transaction'>Conduct a Transaction</Link></div>
+          <div><Link to='/transaction-pool'>Transaction Pool</Link></div>
         <br/>
         <div className='WalletInfo'>
           <div>Address: { address }</div>
